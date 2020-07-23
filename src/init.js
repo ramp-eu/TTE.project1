938fe95dd380eea80266a145a5b1fbc8fe599aec
Jason Fox <jason.fox@fiware.org>
2020-07-23 13:31:39 +0200

    Preparing Docker

--- src/init.js
+++ src/init.js
@@ -3,5 +3,5 @@ const hello = require('./hello');
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end(hello.getText.join(' '));
+    res.end(hello.getText().join(' '));
 }).listen(8080);
